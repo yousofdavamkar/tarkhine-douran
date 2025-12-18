54f50529abc2e258ddba2cbddda15fe0fe71c750
Yousof Davamkar <yousofdavamkar@gmail.com>
2025-12-18 12:45:14 +0330

    install tailwindcss v4.1
    On branch feature/tailwind
    Your branch is up to date with 'origin/feature/tailwind'.
    
    Changes to be committed:
    	modified:   index.html
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/css/styles.css
    	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
     port: 5173,
     strictPort: false,
-    host: true
-  }
-})
+    host: true,
+  },
+  plugins: [tailwindcss()],
+});
