78e96b635955c881aad317c144a2eb4d0989dd90
Amir Hossein Hasanian <hasanianamirhossein@gmail.com>
2026-05-06 17:19:58 +0330

    add json-server package to project and config. add db.json and test json data in this file.

--- src/js/utils/BASE_URL.js
+++ src/js/utils/BASE_URL.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://localhost:3000/api/"
-export const BASE_URL = "https://jsonplaceholder.typicode.com/";
+export const BASE_URL = "http://localhost:3000/";
+// export const BASE_URL = "https://jsonplaceholder.typicode.com/";
