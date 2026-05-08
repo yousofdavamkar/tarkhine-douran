ab4f74246e57b506623c8ec22212ca3d466c17bd
Amir Hossein Hasanian <hasanianamirhossein@gmail.com>
2026-05-08 12:16:31 +0330

    - Added mobile-specific styles and responsive design to FAQ. - Fixed layout issues and minor bugs in the FAQ component.

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,47 +1,46 @@
 import ApiCall from "/src/js/utils/ApiCall.js";
 
-// ساخت نمونه API برای endpoint faqs
 const faqApi = new ApiCall("faqs");
 
-// گرفتن دیتای FAQها و ایجاد HTML
 async function loadFaqs() {
-  const container = document.querySelector(".faq-accordion-container");
+  const container = document.querySelector(".faq__accordion");
 
   const res = await faqApi.get(); // GET /faqs
 
-  const faqs = res.data; // آرایه‌ی داخلی
+  const faqs = res.data;
 
   faqs.forEach((faq) => {
     const item = document.createElement("div");
-    item.classList.add("faq-accordion-item");
+    item.classList.add("faq__accordion-item");
 
     item.innerHTML = `
-  
-              <div class="faq-accordion-question">
-                <p class="accordion-p-question body-xl">
-                 ${faq.question}
-                </p>
-                <svg
-                  class="faq-accordion-icon"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                >
-                  <path
-                    d="M6 9L12 15L18 9"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                  />
-                </svg>
-              </div>
-              <div class="faq-accordion-answer">
-                <p class="body-md">
-                 ${faq.answer}
-                </p>
-              </div>
-            
+          <div class="faq__accordion-question">
+            <p class="faq__accordion-question-text">
+              ${faq.question}
+            </p>
+
+            <svg
+              class="faq__accordion-icon"
+              width="32"
+              height="32"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                d="M6 9L12 15L18 9"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+              />
+            </svg>
+          </div>
+
+          <div class="faq__accordion-answer">
+            <p class="faq__accordion-answer-text body-md">
+              ${faq.answer}
+            </p>
+          </div>
+        
     
     `;
     // آکاردئون (چند تا بتوانند باز باشند)
@@ -53,5 +52,4 @@ async function loadFaqs() {
   });
 }
 
-// اجرای تابع
 loadFaqs();
